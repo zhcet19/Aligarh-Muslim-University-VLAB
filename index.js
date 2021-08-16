d1c5556eda825397222a0aaf41be8f197380a051
faiz alam <faizamu19@gmail.com>
2021-08-16 13:38:46 +0530

    initial commit

---
+++ index.js
@@ -0,0 +1,11 @@
+var express= require("express");
+var bodyParser= require("body-parser");
+
+var app= express();
+app.set("view engine","ejs");
+app.get("/",function(req,res){
+	res.json("trial");
+});
+app.listen(3000, function(){
+    console.log("Welcome you to AMUVLAB");
+  });
