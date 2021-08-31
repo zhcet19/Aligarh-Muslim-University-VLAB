bf3eb0610385bc6129071d3006107bb1a4b1c1f7
Zaidtech <mzali@zhcet.ac.in>
2021-08-31 12:47:40 +0530

    Add simulation page

--- public/Js/demo.js
+++ public/Js/demo.js
@@ -3,6 +3,25 @@ jsPlumb.ready(function () {
     var instance,
         discs = [],
 
+        addDisc = function (evt) {
+            var info = createDisc();
+            var e = prepare(info.id);
+            instance.draggable(info.id);
+            discs.push(info.id);
+            evt.stopPropagation();
+            evt.preventDefault();
+        },
+
+        reset = function (e) {
+            for (var i = 0; i < discs.length; i++) {
+                var d = document.getElementById(discs[i]);
+                if (d) d.parentNode.removeChild(d);
+            }
+            discs = [];
+            e.stopPropagation();
+            e.preventDefault();
+        },
+
         initAnimation = function (elId) {
             var el = document.getElementById(elId);
 
@@ -11,27 +30,20 @@ jsPlumb.ready(function () {
                     jsPlumb.removeClass(elId, "jsPlumb_dragged");
                     return;
                 }
-                var o = instance.getOffset(el, true),
-                    o2 = instance.getOffset(el),
-                    s = jsPlumb.getSize(el),
-                    pxy = [e.pageX || e.clientX, e.pageY || e.clientY],
-                    // c = [pxy[0] - (o.left + (s[0] / 2)), pxy[1] - (o.top + (s[1] / 2))],
-                    // oo = [c[0] / s[0], c[1] / s[1]],
-                    // DIST = 350,
-                    // l = o2.left + (oo[0] * DIST),
-                    t = o2.top + (oo[1] * DIST);
-
-                var id = el.getAttribute("id");
-                instance.animate(el, {left: l, top: t}, { duration: 350, easing: 'easeOutBack' });
+               
             });
         },
 
-        endpoint = {
+    // notice there are no dragOptions specified here, which is different from the
+    // draggableConnectors2 demo.  all connections on this page are therefore
+    // implicitly in the default scope.
+         //--------for voltage connections--------//
+         endpoint = {
             anchor: [0.5, 0.5, 0, -1],
-            connectorStyle: { strokeWidth: 7, stroke: "rgba(118,229,30,0.7)" },
+            connectorStyle: { strokeWidth: 5, stroke: "rgba(255,0,0,1)" },
             endpointsOnTop: true,
             isSource: true,
-            maxConnections: 3,
+            maxConnections: 10,
             isTarget: true,
             dropOptions: { tolerance: "touch", hoverClass: "dropHover" }
         },
@@ -41,38 +53,409 @@ jsPlumb.ready(function () {
 
             return instance.addEndpoint(elId, endpoint);
         },
+//--------for ground connections--------//
+endpoint1 = {
+            anchor: [0.5, 0.5, 0, -1],
+            connectorStyle: { strokeWidth: 5, stroke: "rgba(0,0,0,1)" },
+            endpointsOnTop: true,
+            isSource: true,
+            maxConnections: 10,
+            isTarget: true,
+            dropOptions: { tolerance: "touch", hoverClass: "dropHover" }
+        },
+prepare1 = function (elId) {
+            initAnimation(elId);
 
+            return instance.addEndpoint(elId, endpoint1);
+        },
+
+    // this is overridden by the YUI demo.
+        createDisc = function () {
+            var d = document.createElement("div");
+            d.className = "bigdot";
+            document.getElementById("animation-demo").appendChild(d);
+            var id = '' + ((new Date().getTime()));
+            d.setAttribute("id", id);
+            var w = screen.width - 162, h = screen.height - 200;
+            var x = (5 * w) + Math.floor(Math.random() * (10 * w));
+            var y = (5 * h) + Math.floor(Math.random() * (10 * h));
+            d.style.top = y + 'px';
+            d.style.left = x + 'px';
+            return {d: d, id: id};
+        };
 
     // get a jsPlumb instance, setting some appropriate defaults and a Container.
     instance = jsPlumb.getInstance({
         DragOptions: { cursor: 'wait', zIndex: 20 },
-        Endpoint: [ "Image", { url: "littledot.png"} ],
-        Connector: [ "Bezier", { curviness: 90 } ],
+        Endpoint: [ "Image", { url: "littledot.png" } ],
+        Connector: [ "Bezier", { curviness: -40 } ],
         Container: "canvas"
     });
 
     // suspend drawing and initialise.
     instance.batch(function () {
-        var e1 = prepare("bd1"),
-            e2 = prepare("bd2"),
-            e3 = prepare("bd3"),
-            e4 = prepare("bd4"),
-            e6 = prepare("bd6"),
-            e7 = prepare("bd7"),
-            e8 = prepare("bd8"),
-            e9 = prepare("bd9"),
-            e10 = prepare("bd10"),
-            e11 = prepare("bd11"),
-            clearBtn = jsPlumb.getSelector("#anim-clear"),
+        var e1 = prepare("ld1"),
+            e3 = prepare("ld3"),
+            e4 = prepare("ld4"),
+            e5 = prepare("ld5"),
+            e6 = prepare("ld6"),
+            e7 = prepare("ld7"),
+            e8 = prepare("ld8"),
+            e9 = prepare("ld9"),
+            e10 = prepare("ld10"),
+            e11 = prepare("ld11"),
+            e12 = prepare1("ld12"),
+
+            clearBtn = jsPlumb.getSelector("#delete-connct"),
             addBtn = jsPlumb.getSelector("#add");
 
-        instance.on(clearBtn, "click", function (e) {
-            e.preventDefault();
-            e.stopPropagation();
-            instance.detachEveryConnection();
-        });
+         var detachLinks = jsPlumb.getSelector(".littledot .detach");
+            instance.on(detachLinks, "click", function (e) {
+                instance.deleteConnectionsForElement(this.getAttribute("rel"));
+                jsPlumbUtil.consume(e);
+            });
 
+            //instance.on(document.getElementById("delete-connct"), "click", function (e) {
+               // instance.detachEveryConnection();
+            //showConnectionInfo("");
+               // jsPlumbUtil.consume(e);
+
+            //});
     });
 
     jsPlumb.fire("jsPlumbDemoLoaded", instance);
+
+    document.getElementById("check-button").addEventListener("click", function () {
+        //var d = instance.exportData();
+        //console.log(instance.getAllConnections());
+
+      
+        var correct_connections_1_3 = [
+            {
+                "source": "ld1",
+                "target": "ld3"
+            },
+
+            {
+                "source": "ld3",
+                "target": "ld1"
+            }
+        ];
+
+        var correct_connections_4_7 = [
+            {
+                "source": "ld4",
+                "target": "ld7"
+            },
+
+            {
+                "source": "ld7",
+                "target": "ld4"
+            }
+        ];        
+
+        var correct_connections_8_10 = [
+            {
+                "source": "ld8",
+                "target": "ld10"
+            },
+    
+            {
+                "source": "ld10",
+                "target": "ld8"
+            }
+        ];
+
+        
+
+        var correct_connections_9_12 = [
+            {
+                "source": "ld9",
+                "target": "ld12"
+            },
+
+            {
+                "source": "ld12",
+                "target": "ld9"
+            }
+        ];
+
+        var correct_connections_6_11 = [
+            {
+                "source": "ld6",
+                "target": "ld11"
+            },
+
+            {
+                "source": "ld11",
+                "target": "ld6"
+            }
+        ];
+
+        var correct_connections_5_10 = [
+            {
+                "source": "ld5",
+                "target": "ld10"
+            },
+
+            {
+                "source": "ld10",
+                "target": "ld5"
+            }
+        ];
+		var correct_connections_9_11 = [
+            {
+                "source": "ld9",
+                "target": "ld11"
+            },
+
+            {
+                "source": "ld11",
+                "target": "ld9"
+            }
+        ];
+
+
+
+        //a connection outside this will invalidate the circuit
+        var allowed_connections = [
+            {
+                "source": "ld1",
+                "target": "ld3"
+            },
+    
+            {
+                "source": "ld3",
+                "target": "ld1"
+            },
+            
+            {
+                "source": "ld4",
+                "target": "ld7"
+            },
+
+            {
+                "source": "ld7",
+                "target": "ld4"
+            },
+
+            {
+                "source": "ld9",
+                "target": "ld12"
+            },
+
+            {
+                "source": "ld12",
+                "target": "ld9"
+            },
+            {
+                "source": "ld8",
+                "target": "ld10"
+            },
+
+            {
+                "source": "ld10",
+                "target": "ld8"
+            },
+            {
+                "source": "ld5",
+                "target": "ld10"
+            },
+{
+                "source": "ld10",
+                "target": "ld5"
+            },
+
+            {
+                "source": "ld6",
+                "target": "ld11"
+            },
+			 {
+                "source": "ld11",
+                "target": "ld6"
+            },
+			{
+                "source": "ld9",
+                "target": "ld11"
+            },
+			 {
+                "source": "ld11",
+                "target": "ld9"
+            },
+        ];
+
+        var actual_connections = instance.getAllConnections();
+
+        var is_connected_1_3 = false;
+        var is_connected_4_7 = false;        
+        var is_connected_8_10 = false;
+		 var is_connected_5_10 = false;
+        var is_connected_9_12 = false;
+        var is_connected_9_11 = false;
+        var is_connected_6_11 = false;
+        var unallowed_connection_present = false;
+        var count =0; // counts number of connection
+
+
+        actual_connections.forEach(function (connection) {
+            count++;
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_1_3){
+                is_connected_1_3 = correct_connections_1_3.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                  });
+            }
+
+            if(!unallowed_connection_present){
+                unallowed_connection_present = !(allowed_connections.find(function (conn) {
+                    return (conn.source === this_connection.source && conn.target === this_connection.target);
+                }));
+            }
+            // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+
+        });
+
+        //checking for 4_7 connection
+        actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_4_7){
+                is_connected_4_7 = correct_connections_4_7.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+
+        actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_8_10){
+                is_connected_8_10 = correct_connections_8_10.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+
+        
+
+        actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_9_12){
+                is_connected_9_12 = correct_connections_9_12.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+
+        actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_6_11){
+                is_connected_6_11 = correct_connections_6_11.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+
+        actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_5_10){
+                is_connected_5_10 = correct_connections_5_10.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+		actual_connections.forEach(function (connection) {
+            var this_connection = {
+                "source": connection.sourceId,
+                "target": connection.targetId
+            };
+
+            if(!is_connected_9_11){
+                is_connected_9_11 = correct_connections_9_11.find(function (conn) {
+                    return conn.source === this_connection.source && conn.target === this_connection.target;
+                });
+            }
+              // if this_connection exists in correct_connections
+            // remove this connection from correct ones
+            // continue
+            // else
+            // return false
+        });
+
+        if (is_connected_1_3 && is_connected_4_7 && is_connected_8_10 &&  is_connected_9_12 && is_connected_6_11 && is_connected_5_10 && is_connected_9_11&& !unallowed_connection_present) {
+			
+			
+			 	document.getElementById('add').disabled = false;
+  				document.getElementById('plt').disabled = false;
+ 				document.getElementById('clr').disabled = false;
+	   			
+	  
+            alert("RIGHT CONNECTION \n set resistance and input DC voltage");
+            } else {
+               alert("WRONG CONNECTION");
+                return;
+            }  
+
+
+
+    });
 });
+
+
+
+function deleteconnection(){
+window.location.reload();
+}
+
+
